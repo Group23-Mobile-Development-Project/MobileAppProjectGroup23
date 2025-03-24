ea88fb6e8a18402cfecc3899f3bd9705e812d75c
Roshan Kandel <t3karo02@students.oamk.fi>
2025-03-24 12:37:30 +0200

    Splash MainActivity

--- app/src/main/java/com/example/eventplanner/MainActivity.kt
+++ app/src/main/java/com/example/eventplanner/MainActivity.kt
@@ -108,7 +108,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
                 }
             )
         }) {
-            Text(text = "Google SignIn")
+            Text(text = "Sign in with Google")
         }
     }
 }
